a33b222104331c5559f3299a45b06d9a9876cf5c
Jenna Howieson <jenna@jennahowieson.co.uk>
2022-10-21 14:54:28 +0100

    first 7 tests

---
+++ start_point/practice_page.py
@@ -0,0 +1,81 @@
+class TestPetShop(unittest.TestCase):
+
+    def setUp(self):
+        self.customers = [
+            {
+                "name": "Alice",
+                "pets": [],
+                "cash": 1000
+            },
+            {
+                "name": "Bob",
+                "pets": [],
+                "cash": 50
+            },
+            {
+                "name": "Jack",
+                "pets": [],
+                "cash": 100
+            }
+        ]
+
+        self.new_pet = {
+            "name": "Bors the Younger",
+            "pet_type": "cat",
+            "breed": "Cornish Rex",
+            "price": 100
+        }
+
+        self.cc_pet_shop = {
+            "pets": [
+                {
+                    "name": "Sir Percy",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "King Bagdemagus",
+                    "pet_type": "cat",
+                    "breed": "British Shorthair",
+                    "price": 500
+                },
+                {
+                    "name": "Sir Lancelot",
+                    "pet_type": "dog",
+                    "breed": "Pomsky",
+                    "price": 1000,
+                },
+                {
+                    "name": "Arthur",
+                    "pet_type": "dog",
+                    "breed": "Husky",
+                    "price": 900,
+                },
+                {
+                    "name": "Tristan",
+                    "pet_type": "cat",
+                    "breed": "Basset Hound",
+                    "price": 800,
+                },
+                {
+                    "name": "Merlin",
+                    "pet_type": "cat",
+                    "breed": "Egyptian Mau",
+                    "price": 1500,
+                }
+            ],
+            "admin": {
+                "total_cash": 1000,
+                "pets_sold": 0,
+            },
+            "name": "Camelot of Pets"
+        }
+def get_pets_by_breed (pet_shop,breed):
+    pets_by_breed = [ ]
+    for pet in pet_shop["pets"][0]:
+        if pet_shop["pets"][0]["breed"]== breed:
+            pets_by_breed += pet_shop["pets"][0]["breed"]
+    return pets_by_breed
+
+    Print(get_pets_by_breed)
--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,38 @@
 # WRITE YOUR FUNCTIONS HERE
+# Test 1
+def get_pet_shop_name (pet_shop):
+    name = pet_shop ["name"]
+    return name
+
+# Test 2
+def get_total_cash(pet_shop):
+    total = pet_shop ["admin"]["total_cash"]
+    return total
+
+# Test 3 & 4
+def add_or_remove_cash(pet_shop,add):
+    pet_shop["admin"]["total_cash"]+= add
+    return  get_total_cash
+
+# Test 5 
+def get_pets_sold(pet_shop):
+    pets_sold = pet_shop["admin"]["pets_sold"]
+    return pets_sold
+
+# Test 6 
+def increase_pets_sold (pet_shop, num_sold):
+    pet_shop["admin"]["pets_sold"]+= num_sold
+    return get_pets_sold
+
+#  Test 7 
+def get_stock_count(pet_shop):
+    stock = len(pet_shop["pets"])
+    return stock
+
+# Test 8 
+def get_pets_by_breed (pet_shop,breed):
+    pets_by_breed = [ ]
+    for pet in pet_shop["pets"][0]:
+        if pet_shop["pets"][0]["breed"]== breed:
+            pets_by_breed += pet_shop["pets"][0]["breed"]
+    return pets_by_breed
